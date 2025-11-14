5d3813beba230d19d2d269e76d0de909a404081f
N1nd0tt <oleksiy.lopatynskiy@gmail.com>
2025-11-14 15:31:25 +0100

    DB connected

---
+++ src/main/java/com/example/projekt_zesp/models/User.java
@@ -0,0 +1,23 @@
+package com.example.projekt_zesp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(nullable = false, unique = true)
+    private String id;
+    @Column(nullable = false, unique = true)
+    private String login;
+    @Column(nullable = false)
+    private String password;
+
+}
